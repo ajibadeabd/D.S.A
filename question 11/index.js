5017f52a6ae4898bd2cbd8ff0179becfa0c6e6f7
ajibadeabd <ajibadeabd@gmail.com>
2022-08-08 18:12:09 +0100

    code restructure

--- question 11/index.js
+++ question 11/index.js
@@ -4,10 +4,11 @@ function repeatedString(s, n) {
 let [num,dec] = String((n/s.length).toFixed(2)).split(".")
 let occur = 0
 occur=  s.split("").filter((a)=>a=="a").length * num
-dec = Math.round(Number(`0.${dec}`) * s.length)
-occur = occur + s.slice(0,-(s.length-dec)).split("").filter((num)=>num=="a").length
-return occur
-
+let rem = 0
+if(dec!=0){
+   rem =  s.split("").filter((a,i)=>a=="a" && i<String(dec)).length
+}
+return occur + rem
 }
 
 console.log(repeatedString("aab",882787))
