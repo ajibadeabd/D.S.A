053cd0d08f553b96ab76599ccde6b3c83f80c7d9
ajibadeabd <ajibadeabd@gmail.com>
2022-08-09 22:00:53 +0100

    code optimization

--- question 10/index.js
+++ question 10/index.js
@@ -24,25 +24,20 @@ return count
 function jumpingOnClouds2(c) {
     let  count = 0
     let  position = 0
-
-    for(let i =0;i<c.length;i++){
-        console.log(i)
-        if(position==i){
-            if(c[i+2]==0){
+    for(let i =0;i<c.length-1;i++){
+        if(c[i+2]==0){
             count++
             position=i+2
             i++
         }else if (c[i+1]==0){
              count++
             position=i+1
-            
-        }
         }
     }
-   
 return count
 }
-// ==> O(answer +  1) better than O(n), but still pertially still O(n) ,it saves you time for iteration by skipping some iteration time
-// console.log(jumpingOnClouds2([0,1,0,0,0,1,0]))
-console.log(jumpingOnClouds2([0 ,0 ,1 ,0 ,0 ,1 ,0,0,0,1,0]))  
+console.log(jumpingOnClouds2([0 ,0 ,0 ,0 ,1 ,0]))
+// console.log(jumpingOnClouds2([0,1,0,0,0,1,0,0]))
+
+// console.log(jumpingOnClouds2([0 ,0 ,1 ,0 ,0 ,1 ,0,0,0,1,0]))  
 // console.log(jumpingOnClouds2([0 ,0 ,1 ,0 ,0 ,1 ,0]))  
