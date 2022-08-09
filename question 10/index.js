555c16ff5b23d76b10e04a04b91a030286d91294
ajibadeabd <ajibadeabd@gmail.com>
2022-08-09 09:56:02 +0100

    code update

--- question 10/index.js
+++ question 10/index.js
@@ -42,7 +42,7 @@ function jumpingOnClouds2(c) {
    
 return count
 }
-// ==> O(answer +  1) better than O(n) it saves you time for iteration by skipping some iteration time
+// ==> O(answer +  1) better than O(n), but still pertially still O(n) ,it saves you time for iteration by skipping some iteration time
 // console.log(jumpingOnClouds2([0,1,0,0,0,1,0]))
 console.log(jumpingOnClouds2([0 ,0 ,1 ,0 ,0 ,1 ,0,0,0,1,0]))  
 // console.log(jumpingOnClouds2([0 ,0 ,1 ,0 ,0 ,1 ,0]))  
