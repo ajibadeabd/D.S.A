90d5051919323200231ee39a0285a6c0ad938036
ajibadeabd <ajibadeabd@gmail.com>
2022-08-27 00:10:49 +0100

    added 3 dsa question in java

--- question_3/index.java
+++ question_3/index.java
@@ -1,20 +1,24 @@
-package question_3;
+// package question_3;
 
 
 
-public class index {
+// public class index {
 
-    public static void main(String[] args) {
-       index manyHashes = new index();
-        int staircaseNumber = 6;
-        String hash = "#";
-        for(int j = 0; j<=staircaseNumber ; j++){
-            // manyHashes["w"]=""
+//     public static void main(String[] args) {
+//        index manyHashes = new index();
+//         int staircaseNumber = 6;
+//         Object object = new Object();
+//         String hash = "#";
+//         for(int j = 0; j<=staircaseNumber ; j++){
+//             //  object[j] =  object[j-1] + "";
+//             // object.l=""
+//     System.out.println( object);
 
-        }
-            for(int i = 0; i<staircaseNumber ; i++){
-            hash+="#";
-        }
-    System.out.println( hash );
-    }
-};
+
+//         }
+//             for(int i = 0; i<staircaseNumber ; i++){
+//             hash+="#";
+//         }
+//     // System.out.println( hash );
+//     }
+// };
