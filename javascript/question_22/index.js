7bc4a9e4885bcfa888b07427987542348334f3e2
ajibadeabd <43596899+ajibadeabd@users.noreply.github.com>
2022-12-01 14:41:48 +0100

    solved migratoryBirds

--- javascript/question_22/index.js
+++ javascript/question_22/index.js
@@ -3,11 +3,10 @@ function migratoryBirds(array){
     prev[curr]= !prev[curr]? 1:prev[curr] +1
     return prev
   },{})
-  return Object.entries(ans).sort((a, b) => {
-    return b[1]-a[1]
-  })[0][0]
+  return Object.entries(ans).sort((a, b) => b[1]-a[1])
 }
  console.log(
-migratoryBirds([1,1,2,1,2,3,2,4,6,6])
+migratoryBirds([1,1,2,1,2,3,2,4,6,6])[0][0]
 
  )
+ 
